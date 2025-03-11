6fdc24d2da2bb2eba99eb76e18d265771a0106ea
GABRIEL SOUZA COSTA <24122537@dsn127m1242375.cfp127aluno.local>
2025-03-11 08:58:47 -0300

    Fisrt commit

---
+++ app/src/main/java/br/senai/sp/jandira/bmi/screens/TelaInicial.kt
@@ -0,0 +1,150 @@
+package br.senai.sp.jandira.bmi.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.app.NotificationCompat.Action
+import br.senai.sp.jandira.bmi.R
+
+@Composable
+fun TelaInicial(modifier: Modifier = Modifier) {
+    var nomeState = remember {
+
+        mutableStateOf(value = "")
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.linearGradient(
+                    listOf(
+                        Color(0xFF673AB7),
+                        Color(0xFF000000)
+                    )
+                )
+            ),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo01),
+                contentDescription = stringResource(id = R.string.logo_description),
+                modifier = Modifier
+                    .padding(top = 32.dp)
+            )
+            Text(
+                text = stringResource(R.string.welcome),
+                fontSize = 48.sp,
+                color = Color.White
+            )
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(450.dp),
+                shape = RoundedCornerShape(
+                    topStart = 50.dp,
+                    topEnd = 50.dp
+                )
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(32.dp),
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    horizontalAlignment = Alignment.End
+                ) {
+                    Column {
+                        Text(
+                            text = stringResource(R.string.your_name),
+                            fontSize = 22.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        OutlinedTextField(
+                            value = nomeState.value,
+                            onValueChange = { nome ->
+                                nomeState.value = nome
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 8.dp),
+                            shape = RoundedCornerShape(12.dp),
+                            label = {
+                                Text(
+                                    stringResource(R.string.your_name_here)
+                                )
+                            },
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Default.Email,
+                                    contentDescription = "",
+                                    tint = Color(0xFF673AB7)
+                                )
+                            },
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Text,
+                                capitalization = KeyboardCapitalization.Sentences
+                            )
+                        )
+                    }
+                    Button(onClick = {}) {
+                        Text(
+                            text = stringResource(R.string.next)
+                        )
+                        Icon(
+                            imageVector = Icons.Filled.ArrowForward,
+                            contentDescription = ""
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun TelaInicialPreview() {
+    TelaInicial()
+}
